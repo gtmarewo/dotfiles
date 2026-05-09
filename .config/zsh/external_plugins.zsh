452138b313cbaa61252af1a104b8e24d002d9680
Gerald Marewo <gtmarewo@gmail.com>
2026-05-09 17:01:51 +0200

    Edited ~/.config/zsh/external_plugins.zsh to work on macOS

--- .config/zsh/external_plugins.zsh
+++ .config/zsh/external_plugins.zsh
@@ -4,13 +4,12 @@ echo -n "zsh " # suppress the trailing newline with the option -n
 if [[ $OSTYPE == "linux-gnu"* ]]; then
 	echo "on GNU/linux"
 	export MY_HOMEBREW_PREFIX=/home/linuxbrew/.linuxbrew
-	source $MY_HOMEBREW_PREFIX/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 elif [[ $OSTYPE == "darwin"* ]]; then
 	echo "on macOS"
 	export MY_HOMEBREW_PREFIX=/opt/homebrew
-	source $MY_HOMEBREW_PREFIX/opt/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 fi
 
+source $MY_HOMEBREW_PREFIX/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 source $MY_HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 source $MY_HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $MY_HOMEBREW_PREFIX/share/zsh-you-should-use/you-should-use.plugin.zsh
