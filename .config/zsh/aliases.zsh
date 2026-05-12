3015d493be459a44a8a96c27bcb1f0bee010a83f
Gerald Marewo <gtmarewo@gmail.com>
2026-05-12 17:04:20 +0200

    Edited the macOS aliases for certain apps to be defined ony when the apps exist

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -30,7 +30,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 		alias sar="sudo apt remove"
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	alias visit="/Applications/VisIt.app/Contents/Resources/bin/visit"
-	alias paraview="/Applications/Paraview-6.1.0.app/Contents/MacOS/paraview"
-	alias code='/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Code'
+	[[ -d "/Applications/VisIt.app" ]] && alias visit="/Applications/VisIt.app/Contents/Resources/bin/visit"
+	[[ "$(which paraview)" == *"Paraview"* ]] && alias paraview="/Applications/Paraview-6.1.0.app/Contents/MacOS/paraview"
+	[[ -d "/Applications/Visual Studio Code.app" ]] && alias code='/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Code'
 fi
