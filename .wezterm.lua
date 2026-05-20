d7bc5cf7f97aaf3a3979ea4b9a5c377253afdcb3
Gerald Marewo <gtmarewo@gmail.com>
2026-05-21 00:19:29 +0200

    Added a configuration file for wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,17 @@
+-- Import the wezterm module
+local wezterm = require("wezterm")
+
+-- Store the config_builder in a table called config
+local config = wezterm.config_builder()
+
+-- Choose Nerd Font
+config.font = wezterm.font("CaskaydiaMono Nerd Font")
+config.font_size = 16
+
+-- Remove the tab bar
+config.enable_tab_bar = false
+
+-- Enable the title bar
+config.window_decorations = "TITLE"
+
+return config
