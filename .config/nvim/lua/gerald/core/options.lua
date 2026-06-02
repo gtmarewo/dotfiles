f2098733cf4de466ec5b39d3cca1c8f8664165e0
Gerald Marewo <gtmarewo@gmail.com>
2026-06-02 22:17:42 +0200

    Modified neovim's configuration to include a transparent background and the recommended file structure

---
+++ .config/nvim/lua/gerald/core/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt		-- to be concise
+opt.relativenumber = true	-- show relative number for all but the current line
+opt.number = true		-- show absolute number for current line
+
+opt.ignorecase = true		-- ignore case when searching
+opt.smartcase = true		-- assume case sensitive is wanted if mixed case search is used
+
+opt.termguicolors = true	-- allow for a wider range of colors
+opt.background = "dark"		-- dark mode
+
+local cmd = vim.cmd
+
+cmd("let g:netrw_liststyle = 3")-- tree view from the file explorer
+
+-- Make backgrounds transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
+
+-- cmd("colorscheme peachpuff")
