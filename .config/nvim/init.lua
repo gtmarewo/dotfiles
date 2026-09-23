3bbdbf9c64b3e7d6940588e4f6a7591cd8b9b25c
Gerald Marewo <gtmarewo@gmail.com>
2026-09-23 20:08:27 +0200

    Botched my first attempt at install transparent.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,13 @@
 require("gerald/core/options")
 require("gerald/plugins/colorscheme")
+-- require("gerald/plugins/vim-plug/plugins")
+-- Tell vim-plug where to download your plugins
+-- vim.cmd([[
+--   call plug#begin(stdpath('config') . '/lua/gerald/plugins')
+-- 
+--   " List your plugins here with e.g git-hub-username/repo
+--   Plug 'tpope/vim-sensible'
+--   Plug 'nvim-tree/nvim-web-devicons'
+-- 
+--   call plug#end()
+-- ]])
